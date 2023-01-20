ad16c080eb5d23c8ca8fc0c8c612c3f80db9cb38
luka sakhvadze <lukasakhvadze1@gmail.com>
2023-01-20 13:19:08 +0400

    first

--- index2.js
+++ index2.js
@@ -10,8 +10,12 @@ Form.addEventListener("submit",(event)=>{
     body.innerHTML="<header><h2 class=Page2H2>გმადლობთ🙏🙏🙏</h2> <br><h2 class=Page2H2>🚀🚀🚀თქვენი განცხადება მიღებულია✅✅✅</h2> </header> <br> <main class=SecondClass><h4 class=page2h4><a class=Page2a1 >უკან გადასვლა</a></h4><h4 class=page2h4><a class=Page2a2 href=index.html>მთავარ გვერდზე დაბრუნება</a></h4></main>"
     let returnBack=document.querySelector(".Page2a1")
     returnBack.addEventListener("click",(event)=>{
-        returnBack.classList.toggle("SecondBody")
-        location.reload()
+        let relload=location.reload()
+        relload()
+        if(relload){
+            returnBack.classList.toggle("SecondBody")
+        }
+        
     })
     
 })
